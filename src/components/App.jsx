009ea38a58e0245df0a913fe2d2c198528369b0d
Krishna Tripathy <98588847+KrishnaTripathy@users.noreply.github.com>
2023-11-14 06:15:50 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React,{ useState } from 'react'
+
+function App () {
+  const [Count,setCount] = useState(0);
+   
+  function increase() {
+    setCount(Count + 1);
+  };
+  function decrease()  {
+    setCount(Count-1);
+  };
+  return(
+    <div className="container">
+      <h1>{Count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+};
+export default App
